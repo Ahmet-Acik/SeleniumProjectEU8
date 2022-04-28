c9d9f722c7bfe58e00996f7321d74c4ea952c6b1
Ahmet-Acik <a.acik@icloud.com>
2022-04-28 14:08:10 +0100

    day1 BasicNavigation get(), navigate(), getTitle(), getCurrentURL(), Quite(),

--- src/test/java/com/cydeo/test/day1_selenium_intro/BasicNavigation.java
+++ src/test/java/com/cydeo/test/day1_selenium_intro/BasicNavigation.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // 1 setting up the web driver manager
         WebDriverManager.chromedriver().setup();
@@ -18,9 +18,30 @@ public class BasicNavigation {
         // 3 go to an URl address
 
         driver.get("https://www.tesla.com");
+        // pause execution for 3 second
+
+        System.out.println("Current title getTitle() = " + driver.getTitle());
+        System.out.println("theCurrentURL getCurrentUrl() = " + driver.getCurrentUrl());
+
+        Thread.sleep(2000);
+
+        driver.navigate().back();
+        Thread.sleep(2000);
+
+        driver.navigate().forward();
+        Thread.sleep(2000);
+
+        driver.navigate().to("https://google.com");
+
+        String title = driver.getTitle();
+        System.out.println("Current title getTitle() = " + title);
+
+        String theCurrentURL = driver.getCurrentUrl();
+        System.out.println("theCurrentURL getCurrentUrl() = " + theCurrentURL);
 
 
-
+        //Close the browser
+        driver.quit();
 
 
     }
