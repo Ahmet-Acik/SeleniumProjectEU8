e49712702659a67df91ee2173df9d5a8ff03d6da
Ahmet-Acik <a.acik@icloud.com>
2022-05-19 12:26:12 +0100

    BrowserUtils

--- src/test/java/com/cydeo/test/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/test/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class T1_WindowHandling {
@@ -32,6 +33,23 @@ public class T1_WindowHandling {
         ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
 
         //4. Create a logic to switch to the tab where Etsy.com is open
+
+        Set<String> allWindowHandles = driver.getWindowHandles();
+
+        for (String allWindowHandle : allWindowHandles) {
+
+            driver.switchTo().window(allWindowHandle);
+
+            if(allWindowHandle.contains("etsy")) {
+                break;
+            }
+
+
+        }
+
+
+
+
 
         BrowserUtils.switchWindowAndVerify(driver, "etsy", "Etsy");
     }
