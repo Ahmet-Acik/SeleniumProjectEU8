6ed1404ea5798178dea3948ef9da012dee5aa8d5
Ahmet-Acik <a.acik@icloud.com>
2022-06-11 13:40:12 +0100

    BrowserUtils

---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/SeleniumTest.java
@@ -0,0 +1,41 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+
+        // setting up the web driver manager
+        // WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
+
+        // create instance of the Selenium WebDriver.
+        // WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to a website
+        // driver.get("https://google.com");
+        driver.get(" https://google.com");
+
+        //Maximize current window
+        // driver.manage().window().maximize();
+        driver.manage().window().maximize();
+
+        //Delay execution for 3 seconds to view the maximize operation
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Close the browser
+        driver.quit();
+
+
+    }
+}
+
+
+
--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,10 +1,13 @@
 package com.cydeo.utilities;
 
 /*
-In this class only general utility methods that are not related to some specific page.
+In this class only general utility methods that are NOT related to some specific page.
  */
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.util.Set;
@@ -15,15 +18,14 @@ public class BrowserUtils {
     This method will accept int (in seconds) and execute Thread.sleep
     for given duration
      */
-    public static void sleep(int second) {
-        second *= 1000;
+    public static void sleep(int second){
+            second *=1000;
         try {
             Thread.sleep(second);
-        } catch (InterruptedException e) {
+        }catch (InterruptedException e ) {
 
         }
     }
-
 
     /*
     This method accepts 3 arguments.
@@ -32,7 +34,7 @@ public class BrowserUtils {
         - If condition matches, will break loop.
     Arg3: expectedInTitle to be compared against actualTitle
      */
-    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle) {
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle){
 
         Set<String> allWindowsHandles = driver.getWindowHandles();
 
@@ -42,7 +44,7 @@ public class BrowserUtils {
 
             System.out.println("Current URL: " + driver.getCurrentUrl());
 
-            if (driver.getCurrentUrl().contains(expectedInUrl)) {
+            if (driver.getCurrentUrl().contains(expectedInUrl)){
                 break;
             }
         }
@@ -55,9 +57,20 @@ public class BrowserUtils {
     /*
     This method accepts a String "expectedTitle" and Asserts if it is true
      */
-    public static void verifyTitle(WebDriver driver, String expectedTitle) {
+    public static void verifyTitle(WebDriver driver ,String expectedTitle){
 
         Assert.assertEquals(driver.getTitle(), expectedTitle);
 
     }
+
+    /*
+    Creating a utility method for ExplicitWait, so we don't have to repeat the lines
+     */
+    public static void waitForInvisibilityOf(WebElement webElement){
+        //Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+    }
+
+
 }
